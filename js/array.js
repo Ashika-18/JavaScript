ed544015ba4836b255e61ced3ebb5a1ef44fdda2
ashiba <ashibasama@gmail.com>
2022-12-10 15:35:49 +0900

    next関数

---
+++ js/array.js
@@ -0,0 +1,9 @@
+const userNames = ['侍華子', '侍一郎', '侍二郎', '侍三郎', '侍四郎', '侍五郎'];
+
+console.log(userNames[2]);
+
+userNames[1] = '華子';
+
+userNames[6] = 'たろたろ';
+
+console.log(userNames);
---
+++ js/if-switch.js
@@ -0,0 +1,11 @@
+let num = Math.floor(Math.random() * 5);
+
+console.log(num);
+
+if (num === 4) {
+    console.log('大当たり！😆');
+} else if (num === 3) {
+    console.log('当たりです🧐');
+} else {
+    console.log('ハズレ！');
+}
